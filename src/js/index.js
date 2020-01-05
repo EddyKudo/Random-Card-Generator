f8e1b2bee6d754697272c4f8c168efc90cb76d90
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-01-05 02:13:12 +0000

    Added Card Generator

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-
 import "file-loader?name=[name].[ext]!../index.html";
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("cardVal").innerHTML = generateRandVal();
+  document.getElementsByTagName("h3")[0].innerHTML = generateRandSuit();
 };
+
+function generateRandVal() {
+  let value = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+  let randVal = value[Math.floor(Math.random() * value.length)];
+  return randVal;
+}
+function generateRandSuit() {
+  let suit = ["&diams;", "&hearts;", "&clubs;", "&spades;"];
+  let randSuit = suit[Math.floor(Math.random() * suit.length)];
+  return randSuit;
+}
