cee6c9577abbe652faf1ff4ad84a8260b71cbb9c
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-01-11 01:26:47 +0000

    Completed function to change red and black suit

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,8 @@ import "../style/index.scss";
 
 window.onload = () => {
   document.getElementById("cardVal").innerHTML = generateRandVal();
-  document.getElementsByTagName("h3")[0].innerHTML = generateRandSuit();
+  drawnSuit();
 };
-
 function generateRandVal() {
   let value = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
   let randVal = value[Math.floor(Math.random() * value.length)];
@@ -15,4 +14,18 @@ function generateRandSuit() {
   let suit = ["&diams;", "&hearts;", "&clubs;", "&spades;"];
   let randSuit = suit[Math.floor(Math.random() * suit.length)];
   return randSuit;
+}
+function drawnSuit() {
+  let suitDr = generateRandSuit();
+  let card = document.querySelector("#card");
+  let top = document.querySelector(".suitTop");
+  let bottom = document.querySelector(".suitBottom");
+
+  if (suitDr === "&diams;" || suitDr === "&hearts;") {
+    card.classList.add("red");
+  } else {
+    card.classList.add("black");
+  }
+  top.innerHTML = suitDr;
+  bottom.innerHTML = suitDr;
 }
